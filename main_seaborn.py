db6d68a00c0238772cbcd8c24be900fb7edd1328
Mario Castro <mariocastro.pva@gmail.com>
2021-06-01 14:47:30 -0400

    refactor del codigo en main_seaborn.py

--- main_seaborn.py
+++ main_seaborn.py
@@ -8,33 +8,6 @@ import seaborn as sns # Creación de gráficas y visualización de datos
 import numpy as np
 import matplotlib.pyplot as plt
 
-def estimate_b0_b1(x, y):
-    n = np.size(x)
-    
-
-    #Promedios
-    mean_x, mean_y = np.mean(x), np.mean(y)
-
-    #sumatorias
-    sumatorias_XY = np.sum((x - mean_x)*(y - mean_y))
-    sumatorias_XX = np.sum((x - mean_x)**2)
-
-    
-    #Coeficientes de regresion
-    b1 = sumatorias_XY / sumatorias_XX
-    b0 = mean_y - b1 * mean_x
-
-    return (b0, b1)
-
-def plot_regresion(data, X, Y, b):
-    sns.scatterplot(data=data, x=X, y=Y)
-    
-    prediccion_Y = b[0] + b[1] * X
-    
-    sns.lineplot(data=data, x=X, y=Y)
-
-    plt.show()
-    
 
 def main():
 
@@ -45,15 +18,10 @@ def main():
     Y = df['writing score'].values
 
     X = X.reshape(-1,1)
-    reg = LinearRegression()
-    reg.fit
-    
-    df = pd.read_csv('./studentsperformance.csv')
-    print(df.head())
 
     #Dividir los datos para entrenamiento y prueba
     X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2)
-    
+
     #cargar elmodelo de regresion lineal
     reg = LinearRegression()
     reg.fit(X_train, Y_train)
@@ -64,6 +32,8 @@ def main():
 
     sns.scatterplot(x=x_flat, y=Y_train)
     sns.lineplot(x=x_flat, y=y_hat, color='r')
+
+    #Evaluacion del modelo
 
     y_pred = reg.predict(X_test)
 
@@ -78,18 +48,6 @@ def main():
     print()
     print(value)
     print('error caudratico medio: ',mean_squared_error(Y_test, y_pred))
-
-    plt.show()
-    reg.fit
-
-    X = df['reading score'].values
-    Y = df['writing score'].values
-
-    b = estimate_b0_b1(X,Y)
-
-    print(f'Ĺos valores de b0 = {b[0]} y b1 = {b[1]}')
-
-    plot_regresion(df, X, Y, b)
 
 if __name__ == '__main__':
     main()
